d445857813d59d6666e56d37788209d128c26ffe
Erich Alecsander <Erichalecs@gmail.com>
2022-11-16 22:14:07 -0300

    att mov lampada

--- Ex12/app.js
+++ Ex12/app.js
@@ -10,4 +10,4 @@ function ligar() {
         document.getElementById('lamp').src = imagem_ligado;
     }
 }
-document.getElementById("lamp").addEventListener("click", ligar);
+document.getElementById("lamp").addEventListener("mousemove", ligar);
